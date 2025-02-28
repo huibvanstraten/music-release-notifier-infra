8281f2efba84be94ec9d6f7278b03b765039bcca
huibvanstraten <huib.van.straten@ritense.com>
2025-02-28 08:29:06 +0100

    initial commit

---
+++ src/main/kotlin/com/hvs/musicreleasenotifiercluster/MusicReleaseNotifierClusterApplication.kt
@@ -0,0 +1,11 @@
+package com.hvs.musicreleasenotifiercluster
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MusicReleaseNotifierClusterApplication
+
+fun main(args: Array<String>) {
+    runApplication<MusicReleaseNotifierClusterApplication>(*args)
+}
---
+++ src/test/kotlin/com/hvs/musicreleasenotifiercluster/MusicReleaseNotifierClusterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hvs.musicreleasenotifiercluster
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MusicReleaseNotifierClusterApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
